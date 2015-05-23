276acd1ac58a2629a9112669e5fe5e9252e98d02
davidguaita <davidguaita@gmail.com>
2015-05-24 01:12:10 +0900

    First Commit

---
+++ Ntec/Ntec/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Ntec
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Este es el primer commit");
+		}
+	}
+}
